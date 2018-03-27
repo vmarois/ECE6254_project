097a2346648974d20d08d72b1f51e8228c4a8664
vmarois <vincent.marois@protonmail.com>
2018-03-27 20:52:44 +0200

    Initialize local Python module.

---
+++ helpers/__init__.py
@@ -0,0 +1,12 @@
+# This file is part of ECE6254_project
+"""
+@author: vmarois
+@version: 1.0
+@date: 27/03/2018
+"""
+
+# Import lines for functions in this module
+
+from . import acquisition
+
+from .acquisition import load_mhd_data
---
+++ helpers/acquisition.py
@@ -0,0 +1,39 @@
+# This file is part of ECE6254_project
+"""
+@author: vmarois
+@version: 1.0
+@date: 27/03/2018
+"""
+import SimpleITK as sitk
+import numpy as np
+
+
+def load_mhd_data(filename):
+    """
+    This function reads in a .mhd file, and returns the image (pixel values contained in the associated .raw file),
+    along with some meta-info.
+    :param filename: path to the source file (.mhd)
+    :return: the image array (as a np.ndarray), the origin, the spacing and the dimensions (meta-info)
+    """
+    # reads the image using SimpleITK
+    itkimage = sitk.ReadImage(filename)
+
+    # convert the image to a np.ndarray & shuffle the dimensions to get axis in the order z,y,x
+    image = sitk.GetArrayFromImage(itkimage)
+
+    # if the numpy.ndarray is of shape (1,:,:), convert it to a 2D-array
+    z_len, _, _ = image.shape
+    if z_len == 1:
+        image = image[z_len-1, :, :]
+
+    # read the origin of the image frame
+    origin = np.array(list(reversed(itkimage.GetOrigin())))
+
+    # read the spacing along each dimension (pixel widths)
+    spacing = np.array(list(reversed(itkimage.GetSpacing())))
+
+    # read the images dimensions (order z,y,x)
+    dimensions = np.array(list(reversed(itkimage.GetSize())))
+
+    return image, origin, spacing, dimensions
+
