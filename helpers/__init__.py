617a7bff48506a3b449b50f8095f373dded02c19
vmarois <vincent.marois@protonmail.com>
2018-04-21 12:22:28 +0200

    New function to plot some of the filters of a conv layer.

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -15,4 +15,5 @@ from . import visualization
 from .acquisition import load_mhd_data
 from .augmentation import data_augmentation_pipeline, rotate_dataset, shift_dataset, contrast_stretching_dataset
 from .preprocessing import getRoi, findCenter, findMainOrientation
-from .visualization import plot_train_test_metric, plot_sample, plot_data_augmentation, plot_train_test_metric_kfold
+from .visualization import plot_train_test_metric, plot_sample, plot_data_augmentation, plot_train_test_metric_kfold, \
+    vis_filters
--- helpers/visualization.py
+++ helpers/visualization.py
@@ -9,6 +9,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.models import load_model
+from keras import backend as K
 
 img_rows, img_cols = 128, 128
 scale = 35
@@ -22,6 +23,7 @@ def plot_train_test_metric(train, test, title, metricname):
     :param test: .csv filepath containing metric values evaluated on holdout data
     :param title: string for the plot title & output filename.
     :param metricname: plot ylabel.
+
     :return: None, plot saved to file.
     """
     metric = np.loadtxt(train)
@@ -209,7 +211,7 @@ def plot_sample(model):
 
 def plot_data_augmentation():
     """
-    Plot the original image and the 5 artificially created images side-by-side for visualization.
+    Plot a random original image and the 5 artificially created corresponding images side-by-side for visualization.
 
     :return: plot saved to file.
     """
@@ -220,10 +222,12 @@ def plot_data_augmentation():
     sample = np.random.randint(600)
 
     for idx, file in enumerate(filenames_list):
+
         # load file
         print('Loading', file)
         images = np.load('output/augmented_data/{}.npy'.format(file))
 
+        # create plot
         plt.subplot(2, 3, idx + 1)
         plt.imshow(images[sample], cmap='Greys_r')
         plt.axis('off')
@@ -231,5 +235,107 @@ def plot_data_augmentation():
 
     plt.suptitle('Data Augmentation Example')
     plt.savefig('output/plots/data_augmentation_vis_{}.png'.format(sample), bbox_layout='tight', dpi=300)
+    plt.show()
+    plt.clf()
+
+
+def vis_filters(layer='conv2d_1', img_width=128, img_height=128):
+    """
+    Try to visualize the filters of the specified convolutional filter.
+
+    :param layer: layer name, e.g 'conv2d_1'
+    :param img_height, img_width: input image size
+
+    :return:
+    """
+
+    # load model
+    model = load_model('output/models/cnn_model.h5')
+
+    # the name of the layer we want to visualize
+    layer_name = layer
+
+    # util function to convert a tensor into a valid image
+    def deprocess_image(x):
+        # normalize tensor: center on 0., ensure std is 0.1
+        x -= x.mean()
+        x /= (x.std() + 1e-5)
+        x *= 0.1
+
+        # clip to [0, 1]
+        x += 0.5
+        x = np.clip(x, 0, 1)
+
+        # convert to RGB array
+        x *= 255
+        if K.image_dim_ordering() == 'th':
+            x = x.transpose((1, 2, 0))
+        x = np.clip(x, 0, 255).astype('uint8')
+        return x
+
+    # placeholder for the input images
+    input_img = model.input
+
+    # get the symbolic outputs of each "key" layer
+    layer_dict = dict([(layer.name, layer) for layer in model.layers])
+    layer_output = layer_dict[layer_name].output
+
+    # utility function to normalize a tensor by its L2 norm
+    def normalize(x):
+        return x / (K.sqrt(K.mean(K.square(x))) + 1e-5)
+
+    kept_filters = []
+    for filter_index in range(0, 32):  # 32 filters in first conv layer
+        print('\nProcessing filter %d' % filter_index)
+
+        # build a loss function that maximizes the activation of the ith filter of the layer considered
+        loss = K.mean(layer_output[:, filter_index, :, :])
+
+        # compute the gradient of the input picture wrt this loss
+        grads = K.gradients(loss, input_img)[0]
+
+        # normalization trick: we normalize the gradient
+        grads = normalize(grads)
+
+        # this function returns the loss and grads given the input picture
+        iterate = K.function([input_img], [loss, grads])
+
+        # step size for gradient ascent
+        step = 1.
+
+        # start from a gray image with some random noise
+        input_img_data = np.random.random((1, 1, img_width, img_height))
+        input_img_data = (input_img_data - 0.5) * 20 + 128
+
+        # run gradient ascent for 20 steps
+        for i in range(20):
+            loss_value, grads_value = iterate([input_img_data])
+            input_img_data += grads_value * step
+
+            print('Current loss value:', loss_value)
+
+            # some filters get stuck to 0, we do not take them into account
+            if loss_value <= 0.:
+                print('Negative loss value, skipping current filter.\n')
+                break
+
+        if loss_value > 0:
+            # decode the resulting input image
+            img = deprocess_image(input_img_data[0])
+            kept_filters.append(img)
+        print('Filter %d processed.\n' % filter_index)
+
+    nb_filters = len(kept_filters)
+    print('Number of filters:', nb_filters)
+
+    # Create a plot of the kept filters
+    plt.figure(figsize=(8,8))
+
+    for idx, filter in enumerate(kept_filters):
+        plt.subplot(np.ceil(np.sqrt(nb_filters)), np.ceil(np.sqrt(nb_filters)), idx+1)
+        plt.imshow(filter[0])
+        plt.axis('off')
+    plt.suptitle('Filters with loss > 0 for layer {}'.format(layer))
+    plt.savefig('output/plots/filters_{}_layer.png'.format(layer), bbox_layout='tight', dpi=300)
     plt.show()
     plt.clf()
